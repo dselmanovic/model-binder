03dce04d86f97c2f9c9ad6d0235b1e66b7a9d161
monkeydeveloper <klosterff@gmail.com>
2015-08-02 00:42:28 -0300

    POST support and new convention for resulting binded model.

--- README.md
+++ README.md
@@ -24,10 +24,19 @@ Then in your app just register binder middleware on actions where you need it:
 var binder=require('model-binder');
 
 app.get('/testmodel', binder(require('./person')), function(req, res){
-    console.log(req.requestModel);
+    console.log(req.Person);
     req.requestModel.sayName();
     return res.send(req.requestModel);
 });
 ```
 
+For POST requests, you only have to set body-parser in  your express application or in your post method directly. You can choose json, formurlencoded or both.
+```javascript
+var bodyParser = require('body-parser');
+var binder = require('model-binder');
+var jsonParser = bodyParser.json();
 
+app.post('/testmodel', jsonParser, binder(require('./person')), function(req, res) {
+	console.log(req.person);
+});
+```
--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-function ModelBinderJS (Model){
-    return function(req, res, next){
-        var obj=new Model();
-        for (var key in obj){
-            if ((typeof obj[key])!=='function'){
-                if (req.param(key))
-                    obj[key]=req.param(key);
+function ModelBinderJS(Model) {
+    return function(req, res, next) {
+        var obj = new Model();
+        var params = req.method == 'POST' ? req.body : req.params;
+
+        for (var key in obj) {
+            if ((typeof obj[key]) !== 'function') {
+                if (params[key.toString()])
+                    obj[key] = params[key.toString()];
             }
         }
-        req.requestModel=obj;
+
+        req[Model.name.toString().toLowerCase()] = obj;
         next();
     }
 }
 
-module.exports=ModelBinderJS;
+module.exports = ModelBinderJS;
