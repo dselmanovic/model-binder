2531ccf19a7dfc6b35a49a80230a236a97f4afde
demirselmanovic <ThinkPad1979>
2016-12-26 12:31:13 +0100

    made it case insensitive

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ function ModelBinderJS (Model){
     return function(req, res, next){
         var obj=new Model();
         for (var key in obj){
-            if ((typeof obj[key])!=='function'){
-                if (req.param(key))
-                    obj[key]=req.param(key);
+            if (((typeof obj[key])!=='function') && (req.body)){
+                for (var param in req.body){
+                    if (key.toLowerCase()==param.toLowerCase()){
+                        obj[key]=req.body[param];
+                    }
+                }
             }
         }
         req.requestModel=obj;
